a0274aef1f50711e2ac2cf0506dcf3858dc1a679
Виктория <vika.khalyavina@inbox.ru>
2021-10-12 22:59:12 +0500

    Добавление первых функций третья проверка-доп

--- js/main.js
+++ js/main.js
@@ -7,11 +7,9 @@ getRandomIntFromRange(-10, 10);
 
 function getRandomArbitrary(min, max, maxDigits = 0) {
   const digitsDegree = 10 ** maxDigits;
-  return max <= min
-    ? false
-    : ~~Math.abs(
-        ((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree
-      );
+  const result =
+    ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+  return max <= min ? false : Math.abs(result);
 }
 
 getRandomArbitrary(-10, 10, 5);
